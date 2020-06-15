7c71c504c924cf2fce458754cde167973c49e4ea
Sarthug99 <66769651+Sarthug99@users.noreply.github.com>
2020-06-15 22:18:36 +0530

    Update data_function.py

--- data_function.py
+++ data_function.py
@@ -5,67 +5,79 @@ from werkzeug.utils import secure_filename
 import pandas as pd
 
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
 
-# @app.route("/get_data", methods=['POST', 'GET'])
-#     a = request.args.get('a', type=dict)
-#     b = request.args.get('b', type=dict)
-a = {'All': False, 'non-alert': True, 'alert': False, 'escalations': False}
-b = {'f0': False, 'f1': False, 'f2': True,
-     'f3': False, 'f4': False, 'f5': False, 'f6': False}
-df = pd.read_csv("EQWT_latest_data.csv")
-column = list(df.columns)
-new_columns = []
-if b['f0'] == True:
-    j = 0
-    for k in range(0, len(b) - 1):
-        new_columns.append(column[j])
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    return render_template("uifeaturebackend.html")
+
+
+@app.route("/get_data", methods=['POST', 'GET'])
+def data():
+    received_data = request.get_json(force=True)
+    a = received_data["a"]
+    b = received_data["b"]
+    print(a, b)
+    df = pd.read_csv("EQWT_latest_data.csv")
+    column = list(df.columns)
+    new_columns = []
+    if b['f0'] == True:
+        j = 0
+        for k in range(0, len(b) - 1):
+            new_columns.append(column[j])
         j = j + 1
-else:
-    j = 1
-    for keys in b:
+    else:
         if b['f1'] == True:
-          new_columns.append(column[0])
+            new_columns.append(column[0])
         if b['f2'] == True:
-          new_columns.append(column[1])
+            new_columns.append(column[1])
         if b['f3'] == True:
-          new_columns.append(column[2])
+            new_columns.append(column[2])
         if b['f4'] == True:
-          new_columns.append(column[3])
+            new_columns.append(column[3])
         if b['f5'] == True:
-          new_columns.append(column[4])
+            new_columns.append(column[4])
         if b['f6'] == True:
-          new_columns.append(column[5])
+            new_columns.append(column[5])
 
-          
-        j = j + 1
 
-df_alerts = df[df['has_alerted'] == 1]
-df_nonalerts = df[df['has_alerted'] == 0]
-df_escalations = df[df['has_escalated'] == 1]
-result = {}
-if a['All'] == True:
-    for column in new_columns:
-        result[column] = [list(df_nonalerts[column]), list(
-            df_alerts[column]), list(df_escalations[column])]
+    df_alerts = df[df['has_alerted'] == 1]
+    df_nonalerts = df[df['has_alerted'] == 0]
+    df_escalations = df[df['has_escalated'] == 1]
+    result = {}
+    if a['all'] == True:
+        for column in new_columns:
+            result[column]['non-alerts'] = (list(df_nonalerts[column]))
+            result[column]['alerts'] = (list(df_alerts[column]))
+            result[column]['escalations'] = (list(df_escalations[column]))
 
-else:
-    for column in new_columns:
-        result[column] = []
-        for keys in a:
-            if a[keys] == True:
-                if keys == 'non-alert':
-                    result[column].append(list(df_nonalerts[column]))
-                if keys == 'alert':
-                    result[column].append(list(df_alerts[column]))
-                if keys == 'escalations':
-                    result[column].append(list(df_escalations[column]))
-
-            else:
-                if keys == 'All':
-                    continue()
+    else:
+        for column in new_columns:
+            result[column] = {}
+            for keys in a:
+                if a[keys] == True:
+                    if keys == 'non-alert':
+                        result[column]['non-alerts'] = (list(df_nonalerts[column]))
+                    if keys == 'alert':
+                        result[column]['alerts'] = (list(df_alerts[column]))
+                    if keys == 'escalations':
+                        result[column]['escalations'] = (list(df_escalations[column]))
+                    
                 else:
-                    result[column].append([])
+                    if keys == 'All':
+                        continue
+                    else:
+                        if keys == 'non-alert':
+                            result[column]['non-alerts'] = []
+                        if keys == 'alert':
+                            result[column]['alerts'] = []
+                        if keys == 'escalations':
+                            result[column]['escalations'] = []
 
-print(result)
+
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
