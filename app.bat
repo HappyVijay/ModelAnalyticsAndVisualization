7bdaf1c91aeda04ce5ac105d75e09d56e5ba946f
Radhakuchekar <48984312+Radhakuchekar@users.noreply.github.com>
2020-06-25 16:01:53 +0530

    Batch file for installing required dependencies for dashboar
    Open command prompt in the directory of app.bat file
    run command :   app.bat 
    and then run command: python main.py

---
+++ app.bat
@@ -0,0 +1,10 @@
+python database.py
+pip install pandas
+pip install seaborn 
+pip install ppscore 
+pip install numpy
+pip install -U Werkzeug
+pip install Flask
+pip install -U Jinja2
+pip install -U scikit-learn
+python main.py
